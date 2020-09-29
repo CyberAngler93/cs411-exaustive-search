f1f2d27e2aaa26a1f2d649f7ae17e121aa45686b
mperry37 <mperry37@alaska.edu>
2020-09-28 20:06:19 -0800

    feat: updated function prototypes and commments

--- build.h
+++ build.h
@@ -1,7 +1,8 @@
 //
 // Created by Matt Perry 
-// This contains the protypes for functions required for my soultion to the exaustive search for CS411 Hw2
-// 
+// This contains the prototypes for functions required for my solution to the exhaustive search for CS411 Hw2
+// build.h
+// Last updated 9/28/20
 
 #ifndef CS411_HW2_BUILD_H
 #define CS411_HW2_BUILD_H
@@ -10,7 +11,7 @@
 #include <cmath>
 using Bridge = std::vector<int>;
 int build(int , int , const std::vector<Bridge> &);
-int generate_legal_bridge_sets(const std::vector<std::vector<Bridge>> &, std::vector<std::vector<Bridge>> &);
+int max_toll(const std::vector<std::vector<Bridge>> &subsets);
 void generate_all_subsets(const std::vector<Bridge> &, std::vector<std::vector<Bridge>> &);
 bool compare_bridge_nodes(const int &, const int &, const int &, const int &);
 #endif //CS411_HW2_BUILD_H
